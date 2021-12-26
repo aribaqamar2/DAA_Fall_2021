de89f6d7a5bf74e684b70158f25b8052e02c2d17
Faizan Zahid Siddique <faizan.zahid@koderlabs.com>
2021-12-26 20:53:37 +0500

    add N-Queen Problem

---
+++ script.js
@@ -0,0 +1,78 @@
+let count = 0;
+
+var solveNQueens = function (n) {
+  if(n>3){
+  parseInt(n);
+  console.log("run", n);
+  var res = [];
+  if (n === 1 || n >= 4) dfs(res, [], n, 0);
+  return res;
+  }
+  else{
+    alert("Please Input Number Greater than 3")
+  }
+};
+
+var dfs = function (res, points, n, index) {
+  for (var i = index; i < n; i++) {
+    if (points.length !== i) return;
+    for (var j = 0; j < n; j++) {
+      if (isValid(points, [i, j])) {
+        points.push([i, j]);
+        dfs(res, points, n, i + 1);
+        if (points.length === n) res.push(buildRes(points));
+        console.log({ points });
+        points.pop();
+      }
+    }
+  }
+};
+
+var buildRes = function (points) {
+  var res = [];
+  var n = points.length;
+  for (var i = 0; i < n; i++) {
+    res[i] = "";
+    for (var j = 0; j < n; j++) {
+      res[i] += points[i][1] === j ? "Q" : " ";
+    }
+  }
+  count++;
+  drawBoard(res, count,points);
+  return res;
+};
+
+var drawBoard = function (res, count,points) {
+  console.log("new print array", { res }, count);
+  if (count === 2) {
+    var n = res.length;
+    for (var i = 0; i < n; i++) {
+      var x = document.getElementById("myTable").insertRow(i);
+      res[i] = "";
+      for (var j = 0; j < n; j++) {
+        var y = x.insertCell(j);
+        res[i] +=
+          points[i][1] === j
+            ? (y.classList.add("queen", "box"), "Q")
+            : (y.classList.add("box"), " ");
+        y.innerHTML = res[i][j];
+      }
+    }
+  }
+};
+
+var isValid = function (oldPoints, newPoint) {
+  // console.log("points",oldPoints, newPoint)
+  var len = oldPoints.length;
+  for (var i = 0; i < len; i++) {
+    if (oldPoints[i][0] === newPoint[0] || oldPoints[i][1] === newPoint[1])
+      return false;
+    if (
+      Math.abs(
+        (oldPoints[i][0] - newPoint[0]) / (oldPoints[i][1] - newPoint[1])
+      ) === 1
+    )
+      return false;
+  }
+  return true;
+};
